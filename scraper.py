6762a163de7f6fb8c368d9119cc8c31d30adadcf
Josip <josip.naletilic@netformic.de>
2022-02-04 20:21:23 +0100

    refactoring

--- scraper.py
+++ scraper.py
@@ -17,6 +17,8 @@ wool_df = pd.DataFrame(wool_json)
 product_links = []
 data = []
 
+wool_item = Wool()
+
 
 def get_product_links():
     print('Collecting product links...')
@@ -52,15 +54,14 @@ def get_product_data(urls):
         delivery = soup.find("div", id="ContentPlaceHolder1_upStockInfoDescription").findNext('span').contents[0]
         properties_table = soup.find("div", id="pdetailTableSpecs")
 
-        name = get_name(url)
-        price = get_price(soup)
-        delivery_time = get_delivery_time(delivery)
-        needle_size = get_needle_size(properties_table)
-        composition = get_composition(properties_table)
+        wool_item.get_name(url, wool_df)
+        wool_item.get_price(soup)
+        wool_item.get_delivery_time(get_soup(delivery_url), delivery)
+        wool_item.get_needle_size(properties_table)
+        wool_item.get_composition(properties_table)
 
-        wool_item = Wool(name, price, delivery_time, needle_size, composition)
-
-        data.append(wool_item)
+        data.append(Wool(wool_item.name, wool_item.price, wool_item.delivery_time, wool_item.needle_size,
+                         wool_item.composition))
 
     df = pd.DataFrame([vars(w) for w in data])
 
@@ -73,41 +74,6 @@ def get_soup(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     return soup
-
-
-def get_name(url):
-    global wool_name
-    for x in range(len(wool_df)):
-        w_name = wool_df.name[x].replace(' ', '-').lower()
-        if w_name in url:
-            wool_name = wool_df.name[x]
-    return wool_name
-
-
-def get_price(soup):
-    price = soup.find("span", class_="product-price-amount").text
-    return price
-
-
-def get_delivery_time(delivery):
-    soup = get_soup(delivery_url)
-    if delivery == 'Lieferbar':
-        delivery_table = soup.find('tbody')
-        delivery_time = delivery_table.find("td", text="Gratis Versand").find_next_sibling("td").text
-        print(delivery_time)
-        return delivery_time
-    else:
-        return 'Nicht lieferbar'
-
-
-def get_needle_size(table):
-    needle_size = table.find('td', string="Nadelstärke").findNext('td').contents[0]
-    return needle_size
-
-
-def get_composition(table):
-    composition = table.find('td', string="Zusammenstellung").findNext('td').contents[0]
-    return composition
 
 
 def save_results(df):
--- wool.py
+++ wool.py
@@ -1,8 +1,94 @@
 # creating python class
 class Wool:
-    def __init__(self, name, price, delivery_time, needle_size, composition):
+    def __init__(self, name=None, price=None, delivery_time=None, needle_size=None, composition=None):
+        """
+        Wool class
+
+        Attributes:
+            name
+            price
+            delivery_time
+            needle_size
+            composition
+        """
         self.name = name
         self.price = price
         self.delivery_time = delivery_time
         self.needle_size = needle_size
         self.composition = composition
+
+    def get_name(self, url, df):
+        """
+        Returns wool name.
+
+            Args:
+                url
+                df
+
+            Returns:
+                str: wool name
+        """
+        for x in range(len(df)):
+            w_name = df.name[x].replace(' ', '-').lower()
+            if w_name in url:
+                self.name = df.name[x]
+                # return self.name
+
+    def get_price(self, soup):
+        """
+        Returns wool name.
+
+            Args:
+                soup
+
+            Returns:
+                str: wool price
+        """
+        self.price = soup.find("span", class_="product-price-amount").text + ' €'
+        # return self.price
+
+    def get_delivery_time(self, soup, delivery):
+        """
+        Function to retrieve wool price.
+
+            Args:
+                soup
+                delivery
+
+            Returns:
+                str: delivery time
+        """
+        if delivery == 'Lieferbar':
+            delivery_table = soup.find('tbody')
+            delivery_time = delivery_table.find("td", text="Gratis Versand").find_next_sibling("td").text
+            self.delivery_time = delivery_time
+        else:
+            self.delivery_time = 'Nicht lieferbar'
+
+        # return self.delivery_time
+
+    def get_needle_size(self, table):
+        """
+        Function for wool needle size.
+
+            Args:
+                table
+
+            Returns:
+                str: wool needle size
+        """
+        self.needle_size = table.find('td', string="Nadelstärke").findNext('td').contents[0]
+        # return self.needle_size
+
+    def get_composition(self, table):
+        """
+        Function for wool needle size.
+
+            Args:
+                table
+
+            Returns:
+                str: wool needle size
+        """
+        self.composition = table.find('td', string="Zusammenstellung").findNext('td').contents[0]
+        # return self.composition
