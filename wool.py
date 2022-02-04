754e3059b8355833b57daee8f86a9a26aba9848c
Josip <josip.naletilic@netformic.de>
2022-02-04 17:52:48 +0100

    add Wool Class

---
+++ wool.py
@@ -0,0 +1,8 @@
+# creating python class
+class Wool:
+    def __init__(self, name, price, delivery_time, needle_size, composition):
+        self.name = name
+        self.price = price
+        self.delivery_time = delivery_time
+        self.needle_size = needle_size
+        self.composition = composition
